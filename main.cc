09d50ae8670b440f567561c9fe47230b712f9f35
VEGA <VEGA@localhost.localdomain>
2020-12-16 11:26:14 +0100

    Launching commit

---
+++ main.cc
@@ -0,0 +1,9 @@
+/*! \file main.cc
+ *  \brief File containing the main
+ *  \author Vincent Lefebvre Karim El-Kharroubi
+ *  \version 0.1
+ *  \date 16/12/2020
+ */
+int main(void){
+    return 0;
+}

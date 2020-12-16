6c461201b1ff506dea35e313356668e29bccb1d4
VEGA <VEGA@localhost.localdomain>
2020-12-16 12:19:56 +0100

    Set up testing environment

---
+++ character/character.hh
@@ -0,0 +1,10 @@
+/*! \file character.hh
+ *  \brief The character class
+ *  \author Vincent Lefebvre 
+ *  \version 0.1
+ *  \date 20/12/16
+ */
+#ifndef __CHARACTER_HH__
+#define __CHARACTER_HH__
+
+#endif
